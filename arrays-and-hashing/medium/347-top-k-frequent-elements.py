78c1d62130c2f16db78495379ed067ffbb3667f6
manav-ray <49621723+manav-ray@users.noreply.github.com>
2025-01-04 19:40:14 -0500

    Top k freq

--- arrays-and-hashing/medium/347-top-k-frequent-elements.py
+++ arrays-and-hashing/medium/347-top-k-frequent-elements.py
@@ -1,37 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
-        
-        for n in nums:
-            if n in freq:
-                freq[n] += 1
-            else:
-                freq[n] = 1
-        
-        freqSort = {}
-        
-        for key in freq:
-            currVal = freq[key]
-            
-            if currVal in freqSort:
-                freqSort[currVal].append(key)
-            else:
-                freqSort[currVal] = [key]
-        
-        keylist = sorted(freqSort.keys(), reverse=True)
-        
         res = []
-        
-        for key in keylist:
-            currKeyList = freqSort[key]
-            
-            for j in currKeyList:
-                if len(res) == k:
-                    break
-                
-                res.append(j)
-            
-            if len(res) == k:
-                break
-        
+
+        counts = Counter(nums)
+        heap = [(-count, num) for num, count in counts.items()]
+
+        heapq.heapify(heap)
+
+        while k > 0:
+            res.append(heapq.heappop(heap)[1])
+            k -= 1
+
         return res
