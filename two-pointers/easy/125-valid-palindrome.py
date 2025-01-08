9d149d296cfc47c823248374c059041b2aa6a8e1
Manav Ray <manav.ray@Manavs-Mac.local>
2025-01-07 22:28:46 -0500

    Reverse string + valid palindrome

--- two-pointers/easy/125-valid-palindrome.py
+++ two-pointers/easy/125-valid-palindrome.py
@@ -1,14 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = ''.join(ch for ch in s if ch.isalnum()).lower()
-        
-        start = 0
-        end = len(s) - 1
-        
-        while start < end:
-            if s[start] != s[end]:
+        l, r = 0, len(s) - 1
+
+        while l <= r:
+            if not s[l].isalnum():
+                l += 1
+                continue
+            
+            if not s[r].isalnum():
+                r -= 1
+                continue
+
+            if s[l].lower() != s[r].lower():
                 return False
-            start += 1
-            end -= 1
-        
+            
+            l += 1
+            r -= 1
+            
         return True
