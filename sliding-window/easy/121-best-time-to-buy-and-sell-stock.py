f10d6f7f8549ae240f339820d7c032c1f6aacd95
manav-ray <49621723+manav-ray@users.noreply.github.com>
2025-01-11 14:34:06 -0500

    121

--- sliding-window/easy/121-best-time-to-buy-and-sell-stock.py
+++ sliding-window/easy/121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         res = 0
-        minStock = 100000
-        
-        for stock in prices:
-            minStock = min(stock, minStock)
-            res = max(res, stock-minStock)
-        
+        l, r = 0, 0
+
+        while r < len(prices):
+            if prices[l] > prices[r]:
+                l = r
+                r += 1
+                continue
+            res = max(res, prices[r] - prices[l])
+            r += 1 
+
+
         return res
