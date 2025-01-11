a2428fc6b294515b361936923b4b3ddf08ef8f12
manav-ray <49621723+manav-ray@users.noreply.github.com>
2025-01-10 23:20:17 -0500

    26

---
+++ two-pointers/easy/26-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        l, r = 0, 0
+
+        while r < len(nums):
+            while r < len(nums) and nums[l] == nums[r]:
+                r += 1
+            
+            l += 1
+
+            if l < len(nums) and r < len(nums):
+                nums[l] = nums[r]
+            
+        
+        return l
