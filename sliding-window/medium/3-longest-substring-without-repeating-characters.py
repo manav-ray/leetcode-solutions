f3c513ddcf8f557c872b40d6135be9e888b518a1
manav-ray <49621723+manav-ray@users.noreply.github.com>
2025-01-13 00:38:51 -0500

    3

--- sliding-window/medium/3-longest-substring-without-repeating-characters.py
+++ sliding-window/medium/3-longest-substring-without-repeating-characters.py
@@ -1,17 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         res = 0
-        
         l, r = 0, 0
-        seen = set()
-        
+        dupes = set()
+
         while r < len(s):
-            if s[r] not in seen:
-                seen.add(s[r])
+            if s[r] not in dupes:
+                dupes.add(s[r])
                 r += 1
-                res = max(res, len(seen))
+                res = max(res, (r - l))
             else:
-                seen.remove(s[l])
+                dupes.remove(s[l])
                 l += 1
-        
+
+
         return res
