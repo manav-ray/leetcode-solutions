741171e86e5db77f1b652bd27a8c44ab1a7587ec
manav-ray <49621723+manav-ray@users.noreply.github.com>
2025-02-08 00:50:39 -0500

    2185

---
+++ meta/easy/2185-counting-words-with-a-given-prefix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        res = 0
+
+        for w in words:
+            if len(pref) > len(w):
+                continue
+            
+            if w[0:len(pref)] == pref:
+                res += 1
+
+        return res
